e7a89d472f11f4a162f27912c1ffd36c5e6eb7c3
a-whitehouse <61844534+a-whitehouse@users.noreply.github.com>
2021-11-29 22:28:13 +0000

    Add insertion of shapes by mouseover

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const grid = document.querySelector(".grid")
+const shapesMenu = document.querySelector("#shapes")
 const timeIntervalSlider = document.querySelector("#timeInterval")
 const startButton = document.querySelector("#start")
 const resetButton = document.querySelector("#reset")
-const examplesButton = document.querySelector("#examples")
 const width = 100
 const height = 100
 const totalCells = width * height
@@ -18,7 +18,8 @@ function populateGrid() {
         var cell = document.createElement("div")
         cell.setAttribute("id", i)
         cell.classList.add("cell", "dead")
-        cell.addEventListener("click", switchClickedCellState)
+        cell.addEventListener("mouseover", highlightCells)
+        cell.addEventListener("click", switchHighlightedCells)
         grid.appendChild(cell)
     }
 }
@@ -28,7 +29,35 @@ const cells = document.querySelectorAll(".cell")
 
 startButton.addEventListener("click", pauseGame)
 resetButton.addEventListener("click", resetGrid)
-examplesButton.addEventListener("click", addExamplePatterns)
+
+var highlightedCells = []
+
+function highlightCells(event) {
+    // Unselect previously highlighed cells
+    for (let i = 0; i < highlightedCells.length; i++) {
+        cells[highlightedCells[i]].classList.remove("highlighted")
+    }
+    highlightedCells = []
+
+    let shape = shapes[shapesMenu.value]
+    let startingCellId = parseInt(event.currentTarget.getAttribute("id"))
+
+    for (let i = 0; i < shape.length; i++) {
+        let cellId = startingCellId + parseInt(shape[i])
+        if (cellId < cells.length) {
+            let cell = cells[cellId]
+            cell.classList.add("highlighted")
+            highlightedCells.push(cellId)
+        }
+    }
+}
+
+function switchHighlightedCells() {
+    for (let i = 0; i < highlightedCells.length; i++) {
+        let cell = cells[highlightedCells[i]]
+        switchCellState(cell)
+    }
+}
 
 function resetGrid() {
     clearInterval(intervalId)
@@ -61,45 +90,6 @@ timeIntervalSlider.oninput = () => {
     clearInterval(intervalId)
     intervalId = setInterval(executeGeneration, timeInterval)
 }
-
-function addExamplePatterns() {
-    // Glider
-    switchCellState(cells[101])
-    switchCellState(cells[202])
-    switchCellState(cells[203])
-    switchCellState(cells[301])
-    switchCellState(cells[302])
-
-
-    // Blinker
-    switchCellState(cells[444])
-    switchCellState(cells[445])
-    switchCellState(cells[446])
-
-    // Toad
-    switchCellState(cells[1444])
-    switchCellState(cells[1445])
-    switchCellState(cells[1446])
-    switchCellState(cells[1543])
-    switchCellState(cells[1544])
-    switchCellState(cells[1545])
-
-    // Beacon
-    switchCellState(cells[464])
-    switchCellState(cells[465])
-    switchCellState(cells[564])
-    switchCellState(cells[565])
-    switchCellState(cells[666])
-    switchCellState(cells[667])
-    switchCellState(cells[766])
-    switchCellState(cells[767])
-}
-
-function switchClickedCellState(event) {
-    let cell = event.currentTarget
-    switchCellState(cell)
-}
-
 
 function switchCellState(cell) {
     if (cell.classList.contains("dead")) {
@@ -205,3 +195,46 @@ function completeTransition(cell) {
     }
 }
 
+const shapes = {
+    "Blinker": [0, width, width * 2],
+    "Square": [0, 1, width, width + 1],
+    "Gosper's Glider Gun": [
+        25,
+        width + 23,
+        width + 25,
+        width * 2 + 13,
+        width * 2 + 14,
+        width * 2 + 21,
+        width * 2 + 22,
+        width * 2 + 35,
+        width * 2 + 36,
+        width * 3 + 12,
+        width * 3 + 16,
+        width * 3 + 21,
+        width * 3 + 22,
+        width * 3 + 35,
+        width * 3 + 36,
+        width * 4 + 1,
+        width * 4 + 2,
+        width * 4 + 11,
+        width * 4 + 17,
+        width * 4 + 21,
+        width * 4 + 22,
+        width * 5 + 1,
+        width * 5 + 2,
+        width * 5 + 11,
+        width * 5 + 15,
+        width * 5 + 17,
+        width * 5 + 18,
+        width * 5 + 23,
+        width * 5 + 25,
+        width * 6 + 11,
+        width * 6 + 17,
+        width * 6 + 25,
+        width * 7 + 12,
+        width * 7 + 16,
+        width * 8 + 13,
+        width * 8 + 14
+    ]
+
+}
